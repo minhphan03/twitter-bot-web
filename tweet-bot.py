bd5a2462fcf878b8aa37f11b34998d091ddbb5f3
Minh Phan <anhminh1993@icloud.com>
2021-01-16 13:56:55 -0800

    these are the new files I added to the repository

---
+++ tweet-bot.py
@@ -0,0 +1,30 @@
+from credentials import *
+import tweepy
+import random
+import os
+from os import environ
+from time import sleep
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_TOKEN = environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+#open-file
+
+with open('list.txt') as f:
+    lines = [line.rstrip('\n') for line in f]
+
+shuffled_lines = random.sample(lines, len(lines))
+
+for line in shuffled_lines:
+    try:
+        print(line)
+        api.update_status(line)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+    sleep(900)
