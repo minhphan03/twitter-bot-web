0d3a7c8be2c574108adb6ebf3a300e016fd28f6b
Minh Phan <anhminh1993@icloud.com>
2021-04-09 14:04:28 -0700

    fix attempt 5

--- tweet-bot.py
+++ tweet-bot.py
@@ -3,7 +3,7 @@ import tweepy
 import random
 from os import environ
 from time import sleep
-'''
+
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
@@ -11,7 +11,7 @@ ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-'''
+
 #open the list of the words
 
 def tweeter():
