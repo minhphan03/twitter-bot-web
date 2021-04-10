5c5acb4fc51a259576c073761ade50a07590d027
Minh Phan <anhminh1993@icloud.com>
2021-04-10 13:15:54 -0700

    maintenance

--- tweet-bot.py
+++ tweet-bot.py
@@ -12,19 +12,66 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+def tweeter():
+    while True:
+        with open('list.txt') as f:
+            lines = [line.rstrip('\n') for line in f if line != '\n']
 
-while True:
-    with open('list.txt') as f:
-        lines = [line.rstrip('\n') for line in f if line != '\n']
+        shuffled_lines = random.sample(lines, len(lines))
 
-    shuffled_lines = random.sample(lines, len(lines))
+        for line in shuffled_lines:
+            try:
+                print(line)
+                api.update_status(line)
 
-    for line in shuffled_lines:
-        try:
-            print(line)
-            api.update_status(line)
+            except tweepy.TweepError as e:
+                print(e.reason)
+            sleep(21600)
 
-        except tweepy.TweepError as e:
-            print(e.reason)
-        sleep(21600)
+def retweeter():
+    while True:
+        data = api.user_timeline(screen_name='MerriamWebster', count=7)
+        for tweet in data:
+            try:
+                if "#WordOfTheDay" in tweet.text:
+                    print(tweet.text)
+                    tweet.retweet()
+                    break
+            except tweepy.TweepError as e:
+                print(e)
+        sleep(28800)
 
+        data2 = api.user_timeline(screen_name='Dictionarycom', count=7)
+
+        for tweet in data2:
+            try:
+                if "#WordOfTheDay" in tweet.text:
+                    print(tweet.text)
+                    tweet.retweet()
+                    break
+            except tweepy.TweepError as e:
+                print(e)
+
+        sleep(28800)
+
+        data3 = api.user_timeline(screen_name='Thesauruscom', count=7)
+
+        for tweet in data3:
+            try:
+                if "#SynonymOfTheDay" in tweet.text:
+                    print(tweet.text)
+                    tweet.retweet()
+                    break
+
+            except tweepy.TweepError as e:
+                print(e)
+        sleep(28800)
+
+if __name__ == "__main__":
+    p1 = Process(target=tweeter())
+    p2 = Process(target=retweeter())
+
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
