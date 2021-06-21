f6537adb974303ac0e523f26dc043d21a1418af3
Minh Phan <anhminh1993@icloud.com>
2021-06-20 20:55:15 -0700

    continue development

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, json
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -8,7 +8,12 @@ app.static_folder = 'static'
 def main():
     return render_template("form.html")
 
-
+#after form submission
+@app.route("/result", methods=['POST'])
+def parse_content():
+    _word = request.form['word']
+    _def = request.form['definition']
+    return "<h1>hello</h1>"
 
 if __name__ == "__main__":
     app.run()
